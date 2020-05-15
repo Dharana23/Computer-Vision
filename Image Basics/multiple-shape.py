9e8bd3c40f0b3bb0a3fbe7692293c3b99b5276a1
Dharana <47414442+Dharana23@users.noreply.github.com>
2020-05-15 23:15:04 +0530

    Update multiple-shape.py

--- Image Basics/multiple-shape.py
+++ Image Basics/multiple-shape.py
@@ -5,7 +5,7 @@ def draw_circle(event, x, y, flags, param):
 	if event == cv2.EVENT_LBUTTONDOWN:
 		cv2.circle(img, (x, y), 100, (0, 0, 255), -1)
 	elif event == cv2.EVENT_RBUTTONDOWN:
-		cv2.circle(img, (x, y), 100, (0, 0, 255), -1)
+		cv2.circle(img, (x, y), 100, (0, 255, 0), -1)
 
 img = np.zeros((512, 512, 3), np.uint8)
 
