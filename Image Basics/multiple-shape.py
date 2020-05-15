4de898c6b01ddeef1164a648df0d5cb04d4e6821
Dharana <47414442+Dharana23@users.noreply.github.com>
2020-05-15 21:35:05 +0530

    Add files via upload

---
+++ Image Basics/multiple-shape.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+
+def draw_circle(event, x, y, flags, param):
+	if event == cv2.EVENT_LBUTTONDOWN:
+		cv2.circle(img, (x, y), 100, (0, 0, 255), -1)
+	elif event == cv2.EVENT_RBUTTONDOWN:
+		cv2.circle(img, (x, y), 100, (0, 0, 255), -1)
+
+img = np.zeros((512, 512, 3), np.uint8)
+
+cv2.namedWindow(winname = 'my_drawing')
+cv2.setMouseCallback('my_drawing', draw_circle)
+
+while True:
+	cv2.imshow('my_drawing', img)
+	if cv2.waitKey(20) & 0xFF == 27:
+		break
+
+cv2.destroyAllWindows()
