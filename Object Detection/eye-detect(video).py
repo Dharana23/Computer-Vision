c1868d016143f48dfbf93b433c7e5fdee1cf08a2
Dharana <47414442+Dharana23@users.noreply.github.com>
2020-05-23 00:02:02 +0530

    Add files via upload

---
+++ Object Detection/eye-detect(video).py
@@ -0,0 +1,28 @@
+import cv2
+import numpy as np
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+def detect_eye(img):
+	face_img = img.copy()
+	eye_rects = eye_cascade.detectMultiScale(face_img, scaleFactor=1.2, minNeighbors=5)
+	
+	for(x, y, w, h) in eye_rects:
+		cv2.rectangle(face_img, (x, y), (x+w, y+h), (255, 255, 255), 10)
+	
+	return face_img
+
+cap = cv2.VideoCapture(0)
+
+while True:
+	ret, frame = cap.read(0)
+	frame = detect_eye(frame)
+	cv2.imshow('Video Face Detect', frame)
+	k = cv2.waitKey(1)
+
+	if k == 27:
+		break
+
+cap.release()
+cv2.destroyAllWindows()
