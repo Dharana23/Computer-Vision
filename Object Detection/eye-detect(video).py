faed7a5323e45f5e7c966f171b63890f390af211
Dharana <47414442+Dharana23@users.noreply.github.com>
2020-05-23 17:36:22 +0530

    Update eye-detect(video).py

--- Object Detection/eye-detect(video).py
+++ Object Detection/eye-detect(video).py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 def detect_eye(img):
